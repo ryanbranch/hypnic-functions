92d8ca5bfc2dd2ca96d53aa1f4244abf2e07a452
Ryan <ryanbranch@users.noreply.github.com>
2020-03-26 03:53:24 -0400

    Create dimension-container.py
    dimension-container.py with its DimensionContainer object will be used to define, store and update various dimensions of GUI elements
    
    Also deleted image_manipulator.py as it was unused

---
+++ dimension-container.py
@@ -0,0 +1,44 @@
+import math
+
+class DimensionContainer():
+
+    def __init__(self):
+        self.defaultInternalPadding = 0
+        self.defaultExternalPadding = 0
+
+        self.windowPadX = self.defaultInternalPadding
+        self.windowPadY = self.defaultInternalPadding
+        self.windowWidth = 1800
+        self.windowHeight = 1010
+
+        self.topToolbarPadX = self.defaultInternalPadding
+        self.topToolbarPadY = self.defaultInternalPadding
+        self.topToolbarHeight = 60
+
+        self.bottomToolbarPadX = self.defaultInternalPadding
+        self.bottomToolbarPadY = self.defaultInternalPadding
+        self.bottomToolbarHeight = 60
+
+        self.bottomInfobarPadX = self.defaultInternalPadding
+        self.bottomInfobarPadY = self.defaultInternalPadding
+        self.bottomInfobarHeight = 30
+
+        self.mainContentPadX = self.defaultInternalPadding
+        self.mainContentPadY = self.defaultInternalPadding
+        self.mainContentHeight = self.windowHeight - self.windowPadY - self.topToolbarHeight - self.bottomToolbarHeight - self.bottomInfobarHeight
+
+        self.imagesFramePadX = self.defaultInternalPadding
+        self.imagesFramePadY = self.defaultInternalPadding
+        self.imagesFrameHeight = self.mainContentHeight - self.mainContentPadY
+        self.imagesFrameWidth = self.imagesFrameHeight
+
+        self.imageFramePadX = self.defaultInternalPadding
+        self.imageFramePadY = self.defaultInternalPadding
+        self.imageFrameWidth = math.floor((self.imagesFrameWidth - self.imagesFramePadX) / 2)
+        self.imageFrameHeight = math.floor((self.imagesFrameHeight - self.imagesFramePadY) / 2)
+        self.print(self.imagesFrameWidth, self.imagesFrameHeight, self.imageFrameWidth, self.imageFrameHeight)
+
+        self.controlFramePadX = self.defaultInternalPadding
+        self.controlFramePadY = self.defaultInternalPadding
+        self.controlFrameWidth = self.windowWidth - self.windowPadX - self.imagesFrameWidth
+        self.controlFrameHeight = self.imagesFrameHeight
